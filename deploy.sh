4b7541c0eade9ec1a416aa31ed2a9f984b6b31d2
yuhuiyuan <1370554829@qq.com>
2023-01-30 08:54:30 +0800

    修改git地址

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
                                                                                           
-push_addr=git@github.com:yuan-19/StarYan-blog.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:yuan-19/StarYan-blog.git
+push_addr=`git@github.com:yuan-19/StarYan-blog.git` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:yuan-19/StarYan-blog.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 push_branch=main # 推送的分支
